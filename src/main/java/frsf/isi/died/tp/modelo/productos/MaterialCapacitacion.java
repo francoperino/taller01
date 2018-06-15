e797603245f818171eb652414f090d5659e03dc3
unknown <st@labsis.frsf.utn.edu.ar>
2018-06-14 21:11:13 -0300

    taller4 paso 3

--- src/main/java/frsf/isi/died/tp/modelo/productos/MaterialCapacitacion.java
+++ src/main/java/frsf/isi/died/tp/modelo/productos/MaterialCapacitacion.java
@@ -125,19 +125,45 @@ public abstract class MaterialCapacitacion implements Ordenable, Comparable<Mate
 	
 
 	@Override
-	public boolean equals(Object obj) {
+	/*public boolean equals(Object obj) {
 		
 	     if(obj instanceof MaterialCapacitacion){
 	    return((MaterialCapacitacion) obj).getTitulo().equalsIgnoreCase(this.getTitulo()); 
 	     
 	     }else return false;
-	}
+	}*/
+	
 
 	public String toString() {
 		return ("Titulo: "+this.titulo+"; Precio: "+this.precio());
 		
 	}
 	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((titulo == null) ? 0 : titulo.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		MaterialCapacitacion other = (MaterialCapacitacion) obj;
+		if (titulo == null) {
+			if (other.titulo != null)
+				return false;
+		} else if (!titulo.equals(other.titulo))
+			return false;
+		return true;
+	}
+
 	@Override
 	public final int valor() {
 		
